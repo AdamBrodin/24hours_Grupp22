e275aa2fa28bf11061b7944019296372e3831e16
Oliver Jensen <oliver.jensen@elev.ga.lbs.se>
2019-01-23 15:02:11 +0100

    Shooting

---
+++ Assets/Shooting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject Bullet;
+    [Space]
+    public float bulletspeed;
+    public float firerate;
+    private float nextfire;
+
+    private void FixedUpdate()
+    {
+        FaceMouse();
+        shooting();
+    }
+
+    void FaceMouse()
+    {
+
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 3);
+    }
+
+    void shooting()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextfire)
+        {
+            nextfire = Time.time + firerate;
+            GameObject projectile = Instantiate(Bullet, transform.position, transform.rotation) as GameObject;
+            projectile.GetComponent<Rigidbody2D>().velocity = projectile.transform.up * bulletspeed;
+        }
+    }
+}
